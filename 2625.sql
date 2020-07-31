abb4d5622b83473464c1add26121107e47425b0b
gsrbr <gsrbr@users.noreply.github.com>
2020-07-30 21:04:25 -0300

    Delete 2625.sql

--- 2625.sql
+++
@@ -1,7 +0,0 @@
-select substr(natural_person.cpf, 1, 3) || '.' ||       
-       substr(natural_person.cpf, 4, 3) || '.' ||       
-       substr(natural_person.cpf, 7, 3) || '-' ||       
-       substr(natural_person.cpf, 10) 
-from customers 
-inner join natural_person 
-on natural_person.id_customers=customers.id;
