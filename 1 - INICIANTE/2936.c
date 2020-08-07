8135f0674511104a434c88015a62de5b72da4af1
gsrbr <gsrbr@users.noreply.github.com>
2020-08-07 10:55:38 -0300

    Create 2936.c

---
+++ 1 - INICIANTE/2936.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a,b,c,d,e,r;
+    
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    
+    
+    r=(a*300)+(b*1500)+(c*600)+(d*1000)+(e*150)+225;
+    
+    printf("%d\n",r);
+    
+ 
+    return 0;
+}
