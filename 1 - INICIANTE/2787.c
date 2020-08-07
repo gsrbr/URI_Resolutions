0a6c4d60f3ef5cd97ab44038949b5469312e2404
gsrbr <gsrbr@users.noreply.github.com>
2020-08-07 07:00:07 -0300

    Create 2787.c

---
+++ 1 - INICIANTE/2787.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int l,c;
+    
+    scanf("%d%d",&l,&c);
+    
+    if (l%2==0){
+        if(c%2==0){
+            printf("1\n");
+        }else{
+            printf("0\n");
+        }
+    }else{
+        if(c%2==0){
+            printf("0\n");
+        }else{
+            printf("1\n");
+        }
+    }
+    
+    
+ 
+    return 0;
+}
